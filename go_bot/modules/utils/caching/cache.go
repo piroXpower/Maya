HarukaNetwork/HarukaX

Implement Admin Cache
Signed-off-by: soekarnohatta <redminote5arom@gmail.com>

go_bot/modules/utils/caching/cache.go
@@ -1,0 +1,24 @@
+package caching
+
+import (
+	"github.com/ATechnoHazard/ginko/go_bot/modules/utils/error_handling"
+	"github.com/allegro/bigcache"
+	"time"
+)
+
+var CACHE *bigcache.BigCache
+
+func InitCache() {
+	config := bigcache.Config{Shards: 1024,
+		LifeWindow:         2 * time.Hour,
+		CleanWindow:        5 * time.Minute,
+		MaxEntriesInWindow: 1000 * 10 * 60,
+		MaxEntrySize:       500,
+		HardMaxCacheSize:   512,
+		OnRemove:           nil,
+		OnRemoveWithReason: nil,
+	}
+	cache, err := bigcache.NewBigCache(config)
+	error_handling.HandleErr(err)
+	CACHE = cache
+}

go_bot/modules/utils/caching/redis.go
@@ -1,0 +1,20 @@
+package caching
+
+import (
+	"github.com/ATechnoHazard/ginko/go_bot"
+	"github.com/go-redis/redis"
+	"time"
+)
+
+var REDIS *redis.Client
+
+func InitRedis() {
+	client := redis.NewClient(&redis.Options{
+		Addr:         go_bot.BotConfig.RedisAddress,
+		Password:     go_bot.BotConfig.RedisPassword,
+		DB:           0,
+		DialTimeout:  time.Second,
+		MinIdleConns: 0,
+	})
+	REDIS = client
+}

go_bot/modules/utils/chat_status/chat_status.go
@@ -23,11 +23,18 @@
 package chat_status
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/ATechnoHazard/ginko/go_bot"
+	"github.com/ATechnoHazard/ginko/go_bot/modules/utils/caching"
 	"github.com/ATechnoHazard/ginko/go_bot/modules/utils/error_handling"
 	"github.com/PaulSonOfLars/gotgbot/ext"
 	"strconv"
 )
+
+type Cache struct {
+	Admin []string `json:"admin"`
+}
 
 func CanDelete(chat *ext.Chat, botId int) bool {
 	k, err := chat.GetMember(botId)
@@ -51,20 +58,23 @@ func IsUserBanProtected(chat *ext.Chat, userId int, member *ext.ChatMember) bool
 	}
 }
 
-func IsUserAdmin(chat *ext.Chat, userId int, member *ext.ChatMember) bool {
-	if chat.Type == "private" || contains(go_bot.BotConfig.SudoUsers, strconv.Itoa(userId)) {
+func IsUserAdmin(chat *ext.Chat, userId int) bool {
+	if chat.Type == "private" {
 		return true
 	}
-	if member == nil {
-		mem, err := chat.GetMember(userId)
-		error_handling.HandleErr(err)
-		member = mem
+
+	admins, err := caching.CACHE.Get(fmt.Sprintf("admin_%v", chat.Id))
+	if err != nil {
+		admincache(chat)
 	}
-	if member.Status == "administrator" || member.Status == "creator" {
+
+	var x Cache
+	_ = json.Unmarshal(admins, &x)
+	if contains(x.Admin, strconv.Itoa(userId)) {
 		return true
-	} else {
-		return false
 	}
+
+	return false
 }
 
 func IsBotAdmin(chat *ext.Chat, member *ext.ChatMember) bool {
@@ -97,7 +107,7 @@ func RequireBotAdmin(chat *ext.Chat, msg *ext.Message) bool {
 }
 
 func RequireUserAdmin(chat *ext.Chat, msg *ext.Message, userId int, member *ext.ChatMember) bool {
-	if !IsUserAdmin(chat, userId, member) {
+	if !IsUserAdmin(chat, userId) {
 		_, err := msg.ReplyText("You must be an admin to perform this action.")
 		error_handling.HandleErr(err)
 		return false
@@ -155,4 +165,19 @@ func contains(s []string, e string) bool {
 		}
 	}
 	return false
+}
+
+func admincache(chat *ext.Chat) {
+	x, err := chat.GetAdministrators()
+	error_handling.HandleErr(err)
+	admins := make([]string, 0)
+
+	for _, y := range x {
+		admins = append(admins, strconv.Itoa(y.User.Id))
+	}
+
+	w := &Cache{admins}
+	z, _ := json.Marshal(w)
+	err = caching.CACHE.Set(fmt.Sprintf("admin_%v", chat.Id), z)
+	error_handling.HandleErr(err)
 }

main.go
@@ -35,6 +35,7 @@ import (
 	"github.com/ATechnoHazard/ginko/go_bot/modules/notes"
 	"github.com/ATechnoHazard/ginko/go_bot/modules/sql"
 	"github.com/ATechnoHazard/ginko/go_bot/modules/users"
+	"github.com/ATechnoHazard/ginko/go_bot/modules/utils/caching"
 	"github.com/ATechnoHazard/ginko/go_bot/modules/utils/error_handling"
 	"github.com/ATechnoHazard/ginko/go_bot/modules/warns"
 	"github.com/ATechnoHazard/ginko/go_bot/modules/welcome"
@@ -54,6 +55,10 @@ func main() {
 
 	// Create database tables if not already existing
 	sql.EnsureBotInDb(u)
+
+	// Prepare Caching Service
+	caching.InitCache()
+	caching.InitRedis()
 
 	// Add module handlers
 	bans.LoadBans(u)

piroXpower/Maya

go_bot/modules/utils/caching/cache.go
@@ -1,0 +1,24 @@
+package caching
+
+import (
+	"github.com/ZerNico/Maya/go_bot/modules/utils/error_handling"
+	"github.com/allegro/bigcache"
+	"time"
+)
+
+var CACHE *bigcache.BigCache
+
+func InitCache() {
+	config := bigcache.Config{Shards: 1024,
+		LifeWindow:         2 * time.Hour,
+		CleanWindow:        5 * time.Minute,
+		MaxEntriesInWindow: 1000 * 10 * 60,
+		MaxEntrySize:       500,
+		HardMaxCacheSize:   512,
+		OnRemove:           nil,
+		OnRemoveWithReason: nil,
+	}
+	cache, err := bigcache.NewBigCache(config)
+	error_handling.HandleErr(err)
+	CACHE = cache
+}

go_bot/modules/utils/caching/redis.go
@@ -1,0 +1,20 @@
+package caching
+
+import (
+	"github.com/ZerNico/Maya/go_bot"
+	"github.com/go-redis/redis"
+	"time"
+)
+
+var REDIS *redis.Client
+
+func InitRedis() {
+	client := redis.NewClient(&redis.Options{
+		Addr:         go_bot.BotConfig.RedisAddress,
+		Password:     go_bot.BotConfig.RedisPassword,
+		DB:           0,
+		DialTimeout:  time.Second,
+		MinIdleConns: 0,
+	})
+	REDIS = client
+}

go_bot/modules/utils/chat_status/chat_status.go
@@ -23,11 +23,18 @@
 package chat_status
 
 import (
-	"github.com/ZerNico/Maya/go_bot"
-	"github.com/ZerNico/Maya/go_bot/modules/utils/error_handling"
+	"encoding/json"
+	"fmt"
 	"github.com/PaulSonOfLars/gotgbot/ext"
+	"github.com/ZerNico/Maya/go_bot"
+	"github.com/ZerNico/Maya/go_bot/modules/utils/caching"
+	"github.com/ZerNico/Maya/go_bot/modules/utils/error_handling"
 	"strconv"
 )
+
+type Cache struct {
+	Admin []string `json:"admin"`
+}
 
 func CanDelete(chat *ext.Chat, botId int) bool {
 	k, err := chat.GetMember(botId)
@@ -51,20 +58,23 @@ func IsUserBanProtected(chat *ext.Chat, userId int, member *ext.ChatMember) bool
 	}
 }
 
-func IsUserAdmin(chat *ext.Chat, userId int, member *ext.ChatMember) bool {
-	if chat.Type == "private" || contains(go_bot.BotConfig.SudoUsers, strconv.Itoa(userId)) {
+func IsUserAdmin(chat *ext.Chat, userId int) bool {
+	if chat.Type == "private" {
 		return true
 	}
-	if member == nil {
-		mem, err := chat.GetMember(userId)
-		error_handling.HandleErr(err)
-		member = mem
+
+	admins, err := caching.CACHE.Get(fmt.Sprintf("admin_%v", chat.Id))
+	if err != nil {
+		admincache(chat)
 	}
-	if member.Status == "administrator" || member.Status == "creator" {
+
+	var x Cache
+	_ = json.Unmarshal(admins, &x)
+	if contains(x.Admin, strconv.Itoa(userId)) {
 		return true
-	} else {
-		return false
 	}
+
+	return false
 }
 
 func IsBotAdmin(chat *ext.Chat, member *ext.ChatMember) bool {
@@ -97,7 +107,7 @@ func RequireBotAdmin(chat *ext.Chat, msg *ext.Message) bool {
 }
 
 func RequireUserAdmin(chat *ext.Chat, msg *ext.Message, userId int, member *ext.ChatMember) bool {
-	if !IsUserAdmin(chat, userId, member) {
+	if !IsUserAdmin(chat, userId) {
 		_, err := msg.ReplyText("You must be an admin to perform this action.")
 		error_handling.HandleErr(err)
 		return false
@@ -155,4 +165,19 @@ func contains(s []string, e string) bool {
 		}
 	}
 	return false
+}
+
+func admincache(chat *ext.Chat) {
+	x, err := chat.GetAdministrators()
+	error_handling.HandleErr(err)
+	admins := make([]string, 0)
+
+	for _, y := range x {
+		admins = append(admins, strconv.Itoa(y.User.Id))
+	}
+
+	w := &Cache{admins}
+	z, _ := json.Marshal(w)
+	err = caching.CACHE.Set(fmt.Sprintf("admin_%v", chat.Id), z)
+	error_handling.HandleErr(err)
 }

main.go
@@ -23,6 +23,9 @@
 package main
 
 import (
+	"github.com/PaulSonOfLars/gotgbot"
+	"github.com/PaulSonOfLars/gotgbot/ext"
+	"github.com/PaulSonOfLars/gotgbot/handlers"
 	"github.com/ZerNico/Maya/go_bot"
 	"github.com/ZerNico/Maya/go_bot/modules/admin"
 	"github.com/ZerNico/Maya/go_bot/modules/bans"
@@ -36,12 +39,10 @@ import (
 	"github.com/ZerNico/Maya/go_bot/modules/notes"
 	"github.com/ZerNico/Maya/go_bot/modules/sql"
 	"github.com/ZerNico/Maya/go_bot/modules/users"
+	"github.com/ZerNico/Maya/go_bot/modules/utils/caching"
 	"github.com/ZerNico/Maya/go_bot/modules/utils/error_handling"
 	"github.com/ZerNico/Maya/go_bot/modules/warns"
 	"github.com/ZerNico/Maya/go_bot/modules/welcome"
-	"github.com/PaulSonOfLars/gotgbot"
-	"github.com/PaulSonOfLars/gotgbot/ext"
-	"github.com/PaulSonOfLars/gotgbot/handlers"
 	"log"
 )
 
@@ -55,6 +56,10 @@ func main() {
 
 	// Create database tables if not already existing
 	sql.EnsureBotInDb(u)
+
+	// Prepare Caching Service
+	caching.InitCache()
+	caching.InitRedis()
 
 	// Add module handlers
 	bans.LoadBans(u)