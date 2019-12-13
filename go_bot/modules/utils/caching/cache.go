43f1588677c28ab43790490646ffb9bb88f21245
ATechnoHazard <amolele@gmail.com>
2019-12-13 16:28:00 +0530

    Persist cache entries for a month before eviction
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- go_bot/modules/utils/caching/cache.go
+++ go_bot/modules/utils/caching/cache.go
@@ -1,16 +1,19 @@
 package caching
 
 import (
-	"github.com/ZerNico/Maya/go_bot/modules/utils/error_handling"
-	"github.com/allegro/bigcache"
 	"time"
+
+	"github.com/ZerNico/Maya/go_bot/modules/utils/error_handling"
+	
+	"github.com/allegro/bigcache"
 )
 
 var CACHE *bigcache.BigCache
 
 func InitCache() {
-	config := bigcache.Config{Shards: 1024,
-		LifeWindow:         2 * time.Hour,
+	config := bigcache.Config{
+		Shards:             1024,
+		LifeWindow:         30 * 24 * time.Hour, // one month
 		CleanWindow:        5 * time.Minute,
 		MaxEntriesInWindow: 1000 * 10 * 60,
 		MaxEntrySize:       500,
