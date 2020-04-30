544c3332d074f8b0b9fa9141a47dd7cb130d39e6
Nico Franke <nico.franke01@gmail.com>
2020-04-30 15:43:06 +0200

    Allow disabling modules
    For now the help page of that module will still show

--- maya_bot/modules/__init__.py
+++ maya_bot/modules/__init__.py
@@ -1,17 +1,23 @@
-from maya_bot import logger
+from maya_bot import logger, CONFIG
+
+NO_LOAD_MODULES = CONFIG["advanced"]["not_load_this_modules"]
 
 
 def list_all_modules():
     from os.path import dirname, basename, isfile
     import glob
 
+    modules = []
     mod_paths = glob.glob(dirname(__file__) + "/*.py")
     all_modules = [
         basename(f)[:-3]
         for f in mod_paths
         if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
     ]
-    return all_modules
+    for module in all_modules:
+        if module not in NO_LOAD_MODULES:
+            modules.append(module)
+    return modules
 
 
 ALL_MODULES = sorted(list_all_modules())
